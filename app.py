369c28097a012e662916e515cc38b6027845376b
Manuelo Salumbides II <manuelosalumbidesii@gmail.com>
2025-06-18 19:06:56 +0800

    update

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def delete_note(note_id):
     write_notes(new_notes)
     return jsonify({'message': 'Note deleted'}), 200
 
-
-if __name__ == '__main__':
+    
+    if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
