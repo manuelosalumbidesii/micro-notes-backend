99658afa99b5b9b4925fb9e57397780b731b793c
Manuelo Salumbides II <manuelosalumbidesii@gmail.com>
2025-06-18 19:09:42 +0800

    fix: clean up formatting and flake8 warnings

--- app.py
+++ app.py
@@ -47,6 +47,5 @@ def delete_note(note_id):
     return jsonify({'message': 'Note deleted'}), 200
 
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
